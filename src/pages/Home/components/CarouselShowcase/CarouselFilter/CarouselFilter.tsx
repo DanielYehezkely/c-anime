245cafc16ea3e5217486ce5051a32daedf27ba21
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-07-02 01:28:30 +0300

    Done with the Filter Bar

--- src/pages/Home/components/CarouselShowcase/CarouselFilter/CarouselFilter.tsx
+++ src/pages/Home/components/CarouselShowcase/CarouselFilter/CarouselFilter.tsx
@@ -1,20 +1,102 @@
-import { Box } from "@mui/material";
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 
-const CarouselFilter:React.FC = () => {
+import { Box, IconButton } from "@mui/material";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { GENRES_FILTER } from "../../../../../constants/globalConstants";
+
+import "./CarouselFilter.css";
+
+const CarouselFilter: React.FC = () => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [isAtStart, setIsAtStart] = useState(true);
+  const [isAtEnd, setIsAtEnd] = useState(false);
+  const [activeFilter, setActiveFilter] = useState("All");
+
+  const checkScrollPosition = () => {
+    if (scrollRef.current) {
+      const { scrollLeft, scrollWidth, clientWidth } = scrollRef.current;
+      setIsAtStart(scrollLeft === 0);
+      setIsAtEnd(scrollLeft + clientWidth === scrollWidth);
+    }
+  };
+
+  const scrollToMax = (direction: "left" | "right") => {
+    if (scrollRef.current) {
+      if (direction === "left") {
+        scrollRef.current.scrollLeft = 0;
+      } else if (direction === "right") {
+        scrollRef.current.scrollLeft =
+          scrollRef.current.scrollWidth - scrollRef.current.clientWidth;
+      }
+      checkScrollPosition();
+    }
+  };
+
+  useEffect(() => {
+    checkScrollPosition();
+    if (scrollRef.current) {
+      scrollRef.current.addEventListener("scroll", checkScrollPosition);
+    }
+    return () => {
+      if (scrollRef.current) {
+        scrollRef.current.removeEventListener("scroll", checkScrollPosition);
+      }
+    };
+  }, []);
+
   return (
     <Box
       component="section"
-      sx={{
-        border: "1px solid white",
-        height: "7.5rem",
-        width: "100%",
-        zIndex: 10,
-      }}
+      className={`carousel-filter-container ${isAtStart ? "at-start" : ""} ${
+        isAtEnd ? "at-end" : ""
+      }`}
+    >
+      <IconButton
+        sx={{
+          height: "100%",
+          display: "flex",
+          alignItems: "flex-start",
+          paddingTop: "1.2rem",
+        }}
+        onClick={() => scrollToMax("left")}
       >
-
-        
-
+        <ArrowBackIosIcon
+          sx={{
+            color: "white",
+            fontSize: "2.5rem",
+            zIndex: 2,
+          }}
+        />
+      </IconButton>
+      <Box component="div" className="carousel-filter" ref={scrollRef}>
+        {GENRES_FILTER.map((genre) => (
+          <Box
+            key={genre}
+            className={`filter-item ${activeFilter === genre ? "active" : ""}`}
+            onClick={() => setActiveFilter(genre)}
+          >
+            {genre}
+          </Box>
+        ))}
+      </Box>
+      <IconButton
+        sx={{
+          height: "100%",
+          display: "flex",
+          alignItems: "flex-start",
+          paddingTop: "1.2rem",
+        }}
+        onClick={() => scrollToMax("right")}
+      >
+        <ArrowForwardIosIcon
+          sx={{
+            color: "white",
+            fontSize: "2.5rem",
+            zIndex: 2,
+          }}
+        />
+      </IconButton>
     </Box>
   );
 };
