e9ad96898fdb400b0035c89fd7805686dba42574
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-07-04 15:47:28 +0300

    Done with the delete

--- src/pages/SingleAnimePage/components/CommentSection/CommentSection.tsx
+++ src/pages/SingleAnimePage/components/CommentSection/CommentSection.tsx
@@ -1,7 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useAuth } from "../../../../context/AuthContext/AuthContext";
-import { Box, Typography } from "@mui/material";
-import { AccountCircleRounded } from "@mui/icons-material";
+import { Box, IconButton, Typography, Avatar, Button, Divider } from "@mui/material";
+import {
+  AccountCircleRounded,
+  MoreVert as MoreVertIcon,
+} from "@mui/icons-material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from "@mui/icons-material/Edit";
+import { getDoc, doc } from "firebase/firestore";
+import { db } from "../../../../config/firebaseConfig";
+import { Timestamp } from "firebase/firestore";
 import {
   CommentsSectionContainer,
   CommentsHeader,
@@ -11,10 +19,7 @@ import {
   SubmitButton,
   NoCommentsBox,
 } from "./CommentSection.styles";
-import { Avatar } from "@mui/material";
-import { getDoc, doc } from "firebase/firestore";
-import { db } from "../../../../config/firebaseConfig";
-import { Timestamp } from "firebase/firestore";
+import "./CommentSection.css"; // Import CSS for custom modal styles
 
 interface Comment {
   userId: string;
@@ -37,9 +42,13 @@ const CommentSection: React.FC<CommentSectionProps> = ({
   liked,
   disliked,
 }) => {
-  const { user, addComment } = useAuth();
+  const { user, addComment, deleteComment } = useAuth();
   const [comment, setComment] = useState("");
   const [userAvatars, setUserAvatars] = useState<{ [key: string]: string }>({});
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedCommentId, setSelectedCommentId] = useState<string | null>(
+    null
+  );
 
   useEffect(() => {
     const fetchUserAvatars = async () => {
@@ -60,6 +69,11 @@ const CommentSection: React.FC<CommentSectionProps> = ({
     fetchUserAvatars();
   }, [comments]);
 
+  const handleModalToggle = (commentId: string) => {
+    setSelectedCommentId(commentId);
+    setIsModalOpen((prev) => !prev);
+  };
+
   const handleSubmit = async (e: React.FormEvent<HTMLDivElement>) => {
     e.preventDefault();
     if (!comment) return;
@@ -71,6 +85,16 @@ const CommentSection: React.FC<CommentSectionProps> = ({
     await addComment(animeId, comment);
     setComments((prevComments) => [...prevComments, newComment]);
     setComment("");
+  };
+
+  const handleDeleteComment = async () => {
+    if (selectedCommentId) {
+      await deleteComment(animeId, selectedCommentId);
+      setComments((prevComments) =>
+        prevComments.filter((comment) => comment.userId !== selectedCommentId)
+      );
+      setIsModalOpen(false);
+    }
   };
 
   return (
@@ -96,36 +120,90 @@ const CommentSection: React.FC<CommentSectionProps> = ({
       )}
       {comments.length > 0 ? (
         comments.map((commentObj, index) => (
-          <Box key={index} display="flex" alignItems="center" mb={2} mt={2}>
-            <Avatar
-              src={
-                userAvatars[commentObj.userId] || "/path/to/default/avatar.png"
-              }
-              alt="user-avatar"
-              sx={{ mr: 2, alignSelf: "flex-start" }}
-            />
-            <Box>
+          <Box
+            key={index}
+            display="flex"
+            alignItems="flex-start"
+            justifyContent="space-between"
+            mb={2}
+            mt={2}
+            position="relative"
+          >
+            <Box display="flex">
+              <Avatar
+                src={
+                  userAvatars[commentObj.userId] ||
+                  "/path/to/default/avatar.png"
+                }
+                alt="user-avatar"
+                sx={{ mr: 2, alignSelf: "flex-start" }}
+              />
               <Box
                 sx={{
                   display: "flex",
-                  alignItems: "center",
+                  flexDirection: "column",
                   gap: "1rem",
                   mb: "1rem",
                 }}
               >
-                <Typography sx={{ color: "white", fontSize: "1.4rem" }}>
-                  {user?.displayName ? user.displayName : user?.email}
-                </Typography>
-                <Typography sx={{ color: "#ffffff83" }}>
-                  {new Date(
-                    commentObj.timestamp.seconds * 1000
-                  ).toLocaleString()}
+                <Box display="flex" alignItems="center" gap="1rem">
+                  <Typography sx={{ color: "white", fontSize: "1.4rem" }}>
+                    {user?.displayName ? user.displayName : user?.email}
+                  </Typography>
+                  <Typography sx={{ color: "#ffffff83" }}>
+                    {new Date(
+                      commentObj.timestamp.seconds * 1000
+                    ).toLocaleString()}
+                  </Typography>
+                </Box>
+                <Typography sx={{ color: "#ffffffe1", fontSize: "1.6rem" }}>
+                  {commentObj.comment}
                 </Typography>
               </Box>
-              <Typography sx={{ color: "#ffffffe1", fontSize: "1.6rem" }}>
-                {commentObj.comment}
-              </Typography>
             </Box>
+            {commentObj.userId === user?.uid && (
+              <IconButton
+                onClick={() => handleModalToggle(commentObj.userId)}
+                sx={{ color: "white" }}
+              >
+                <MoreVertIcon sx={{fontSize: "2rem"}} />
+              </IconButton>
+            )}
+            <div
+              className={`custom-modal ${isModalOpen ? "open" : ""}`}
+            >
+              <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="space-between"
+              >
+                <Button
+                  onClick={handleDeleteComment}
+                  sx={{
+                    color: "white",
+                    fontSize: "1.4rem",
+                    "&:hover": {
+                      bgcolor: "#bd0606d6",
+                    },
+                  }}
+                >
+                  <DeleteIcon />
+                </Button>
+                <StyledDivider />
+                <Button
+                  onClick={() => setIsModalOpen(false)}
+                  sx={{
+                    color: "white",
+                    fontSize: "1.4rem",
+                    "&:hover": {
+                      bgcolor: "#07d823a7",
+                    },
+                  }}
+                >
+                  <EditIcon />
+                </Button>
+              </Box>
+            </div>
           </Box>
         ))
       ) : (
