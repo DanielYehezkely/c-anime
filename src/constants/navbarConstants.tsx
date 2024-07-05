6bca7c1e520ccf22d07e62edd69f3ca22870bffa
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-07-05 17:24:05 +0300

    Removed unrelevant nav links

--- src/constants/navbarConstants.tsx
+++ src/constants/navbarConstants.tsx
@@ -1,9 +1,9 @@
 import {
   Home as HomeIcon,
   LibraryBooks as InfoIcon,
-  Mail as MailIcon,
-  TravelExplore as ExploreIcon,
-  Search as SearchIcon,
+  // Mail as MailIcon,
+  // TravelExplore as ExploreIcon,
+  // Search as SearchIcon,
 } from "@mui/icons-material";
 
 export const VERTICAL_MENU_ITEMS = [
@@ -13,25 +13,25 @@ export const VERTICAL_MENU_ITEMS = [
     icon: <InfoIcon sx={{ fontSize: "2.5rem" }} />,
     path: "/watchlist",
   },
-  {
-    text: "Explore",
-    icon: <ExploreIcon sx={{ fontSize: "2.5rem" }} />,
-    path: "/explore",
-  },
-  {
-    text: "Search",
-    icon: <SearchIcon sx={{ fontSize: "2.5rem" }} />,
-  },
-  {
-    text: "Contact",
-    icon: <MailIcon sx={{ fontSize: "2.5rem" }} />,
-    path: "/contact",
-  },
+  // {
+  //   text: "Explore",
+  //   icon: <ExploreIcon sx={{ fontSize: "2.5rem" }} />,
+  //   path: "/explore",
+  // },
+  // {
+  //   text: "Search",
+  //   icon: <SearchIcon sx={{ fontSize: "2.5rem" }} />,
+  // },
+  // {
+  //   text: "Contact",
+  //   icon: <MailIcon sx={{ fontSize: "2.5rem" }} />,
+  //   path: "/contact",
+  // },
 ];
 
 export const HORIZONTAL_NAVBAR_TITLES = [
   { name: "Home", path: "/" },
   { name: "Watch List", path: "/watchlist" },
-  { name: "Explore", path: "/explore" },
-  { name: "Contact", path: "/contact" },
+  // { name: "Explore", path: "/explore" },
+  // { name: "Contact", path: "/contact" },
 ];
