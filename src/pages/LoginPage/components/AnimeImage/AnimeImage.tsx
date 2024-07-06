b09c4469b8ceb7460beda234c3ebd593796b7192
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-07-06 23:51:18 +0300

    Testing responsive on login form

--- src/pages/LoginPage/components/AnimeImage/AnimeImage.tsx
+++ src/pages/LoginPage/components/AnimeImage/AnimeImage.tsx
@@ -1,28 +1,36 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import React from "react";
+import theme from "../../../../MUI/theme";
 
 const AnimeImage:React.FC = () => {
+  
+  const isXs = useMediaQuery(theme.breakpoints.only("xs"));
+
   return (
     <Box
       position="fixed"
       component="img"
-      src="/assets/images/pngwing.com.png"
+      src={
+        isXs
+          ? "/public/assets/images/Naruto-Akatsuki-PNG-Transparent-Image.png"
+          : "/assets/images/pngwing.com.png"
+      }
       sx={{
         width: {
-          xs: "12rem", // width for extra small screens
+          xs: "rem", // width for extra small screens
           sm: "20rem", // width for small screens
           md: "25rem", // width for medium screens
           lg: "30rem", // width for large screens
           xl: "35rem", // width for extra large screens
         },
         height: {
-          xs: "20rem", // height for extra small screens
+          xs: "10rem", // height for extra small screens
           sm: "30rem", // height for small screens
           md: "40rem", // height for medium screens
           lg: "50rem", // height for large screens
           xl: "60rem", // height for extra large screens
         },
-        bottom: 0,
+        bottom: isXs ? "5rem" : 0 ,
         right: {
           xs: "32%", // right position for extra small screens
           sm: "7%", // right position for small screens
