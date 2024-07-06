6db505828745a7411daf018630745fa9dc128f19
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-07-07 00:06:07 +0300

    Working in responsive for home page

--- src/pages/LoginPage/components/AnimeImage/AnimeImage.tsx
+++ src/pages/LoginPage/components/AnimeImage/AnimeImage.tsx
@@ -19,24 +19,24 @@ const AnimeImage:React.FC = () => {
         width: {
           xs: "50%", // width for extra small screens
           sm: "20rem", // width for small screens
-          md: "25rem", // width for medium screens
-          lg: "30rem", // width for large screens
-          xl: "35rem", // width for extra large screens
+          // md: "25rem", // width for medium screens
+          // lg: "30rem", // width for large screens
+          // xl: "35rem", // width for extra large screens
         },
         height: {
           xs: "20%", // height for extra small screens
           sm: "30rem", // height for small screens
-          md: "40rem", // height for medium screens
-          lg: "50rem", // height for large screens
-          xl: "60rem", // height for extra large screens
+          // md: "40rem", // height for medium screens
+          // lg: "50rem", // height for large screens
+          // xl: "60rem", // height for extra large screens
         },
-        bottom: isXs ? "20%" : 0 ,
+        bottom: isXs ? "5%" : 0 ,
         right: {
-          xs: "40%", // right position for extra small screens
+          xs: "30%", // right position for extra small screens
           sm: "7%", // right position for small screens
-          md: "10%", // right position for medium screens
-          lg: "12%", // right position for large screens
-          xl: "15%", // right position for extra large screens
+          // md: "10%", // right position for medium screens
+          // lg: "12%", // right position for large screens
+          // xl: "15%", // right position for extra large screens
         },
       }}
     />
