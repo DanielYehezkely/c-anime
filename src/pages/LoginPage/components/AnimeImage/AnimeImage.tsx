e1e8bb47c511c9b712bb2142d6c8a43870dd704c
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-07-06 23:40:10 +0300

    Working on responsive

--- src/pages/LoginPage/components/AnimeImage/AnimeImage.tsx
+++ src/pages/LoginPage/components/AnimeImage/AnimeImage.tsx
@@ -8,10 +8,28 @@ const AnimeImage:React.FC = () => {
       component="img"
       src="/assets/images/pngwing.com.png"
       sx={{
-        width: "30rem",
-        height: "50rem",
-        bottom: "0",
-        right: "10%",
+        width: {
+          xs: "15rem", // width for extra small screens
+          sm: "20rem", // width for small screens
+          md: "25rem", // width for medium screens
+          lg: "30rem", // width for large screens
+          xl: "35rem", // width for extra large screens
+        },
+        height: {
+          xs: "23rem", // height for extra small screens
+          sm: "30rem", // height for small screens
+          md: "40rem", // height for medium screens
+          lg: "50rem", // height for large screens
+          xl: "60rem", // height for extra large screens
+        },
+        bottom: 0,
+        right: {
+          xs: "32%", // right position for extra small screens
+          sm: "7%", // right position for small screens
+          md: "10%", // right position for medium screens
+          lg: "12%", // right position for large screens
+          xl: "15%", // right position for extra large screens
+        },
       }}
     />
   );
