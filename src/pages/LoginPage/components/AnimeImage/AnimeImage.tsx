d16e58fe445ef94f5f20b5ab8ddec0a1c9974e6b
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-07-07 00:00:47 +0300

    Done with form login resposive

--- src/pages/LoginPage/components/AnimeImage/AnimeImage.tsx
+++ src/pages/LoginPage/components/AnimeImage/AnimeImage.tsx
@@ -12,27 +12,27 @@ const AnimeImage:React.FC = () => {
       component="img"
       src={
         isXs
-          ? "/public/assets/images/Naruto-Akatsuki-PNG-Transparent-Image.png"
+          ? "/assets/images/Naruto-Akatsuki-PNG-Transparent-Image.png"
           : "/assets/images/pngwing.com.png"
       }
       sx={{
         width: {
-          xs: "rem", // width for extra small screens
+          xs: "50%", // width for extra small screens
           sm: "20rem", // width for small screens
           md: "25rem", // width for medium screens
           lg: "30rem", // width for large screens
           xl: "35rem", // width for extra large screens
         },
         height: {
-          xs: "10rem", // height for extra small screens
+          xs: "20%", // height for extra small screens
           sm: "30rem", // height for small screens
           md: "40rem", // height for medium screens
           lg: "50rem", // height for large screens
           xl: "60rem", // height for extra large screens
         },
-        bottom: isXs ? "5rem" : 0 ,
+        bottom: isXs ? "20%" : 0 ,
         right: {
-          xs: "32%", // right position for extra small screens
+          xs: "40%", // right position for extra small screens
           sm: "7%", // right position for small screens
           md: "10%", // right position for medium screens
           lg: "12%", // right position for large screens
